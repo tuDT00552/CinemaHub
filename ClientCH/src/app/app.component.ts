b8484618151b1ec99cc24c49ef3902e44690328f
Da Tua Tu <tudt00552@fpt.edu.vn>
2020-06-13 13:08:42 +0700

    update template

--- ClientCH/src/app/app.component.ts
+++ ClientCH/src/app/app.component.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import {CinemaService} from './cinema/cinema.service';
-import {Cinema} from './cinema/Cinema';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  cinemas: Cinema[];
-  title = 'Cinema Hub';
-
-  getCinemas(): void {
-    this.cinemaService.getCinemas()
-      .subscribe(cinemas => this.cinemas = cinemas);
-  }
-
-  constructor(private titleService: Title, private cinemaService: CinemaService) {
-    titleService.setTitle(this.title);
-    this.getCinemas();
-  }
-
+  title = 'argon-dashboard-angular';
 }
--- ClientCH/src/app/app.module.ts
+++ ClientCH/src/app/app.module.ts
@@ -1,28 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CinemaComponent } from './cinema/cinema.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { DefaultComponent } from './default/default.component';
-import { MoviesComponent } from './movies/movies.component';
-import { ShowtimesComponent } from './showtimes/showtimes.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+
 
 @NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    NgbModule,
+    RouterModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
-    CinemaComponent,
-    LoginComponent,
-    RegisterComponent,
-    DefaultComponent,
-    MoviesComponent,
-    ShowtimesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    AdminLayoutComponent,
+    AuthLayoutComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
